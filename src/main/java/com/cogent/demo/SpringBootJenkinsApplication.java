fc6f23442062f2d130126f0b76bad8e7ae8c9224
Ravimishracogent <ravi.mishra@cogentinfo.com>
2024-03-04 17:13:32 -0500

    First Commit from IDE

---
+++ src/main/java/com/cogent/demo/SpringBootJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.cogent.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+	static Logger log = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+
+	@PostConstruct
+	void init() {
+		log.debug("Its init post construct statement");
+	}
+
+	public static void main(String[] args) {
+		log.error("Found Error in First line of main");
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
