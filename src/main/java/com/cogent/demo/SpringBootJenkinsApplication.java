cb72ce8e767c7787a9d2cc39644dee2b338d3a14
Ravimishracogent <ravi.mishra@cogentinfo.com>
2024-03-06 10:11:42 -0500

    Updated Code

--- src/main/java/com/cogent/demo/SpringBootJenkinsApplication.java
+++ src/main/java/com/cogent/demo/SpringBootJenkinsApplication.java
@@ -18,7 +18,7 @@ public class SpringBootJenkinsApplication {
 	}
 
 	public static void main(String[] args) {
-		log.error("Found Error in First line of main");
+		log.error("Found Our First Statement from main");
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
 
